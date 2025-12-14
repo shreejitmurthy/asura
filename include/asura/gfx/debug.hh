a84663447ed66ff5e69551615d6e5b67a8465925
Shreejit Murthy <botmorph17@gmail.com>
2025-12-14 22:25:11 +1030

    refactor debug print function to check device debug state

--- include/asura/gfx/debug.hh
+++ include/asura/gfx/debug.hh
@@ -10,12 +10,15 @@
 #include <sokol/util/sokol_debugtext.h>
 #include <sokol/util/sokol_color.h>
 
+#include "device.hh"
+
 #define FONT_KC854 (0)
 #define FONT_ORIC  (1)
 
 namespace Asura::Debug {
 
 inline void print(const std::string &text, sg_color colour = sg_white, int font = FONT_KC854) {
+    if (!Device::instance().debug) return;
     sdtx_font(font);
     sdtx_color3b(colour.r * 255, colour.g * 255, colour.b * 255);
     sdtx_printf("%s\n\n", text.c_str());
