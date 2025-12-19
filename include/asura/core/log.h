6cbf2d2bdff516fae748d4c1eab855f3618934bf
Shreejit Murthy <botmorph17@gmail.com>
2025-12-19 11:06:27 +1030

    add guard & date

--- include/asura/core/log.h
+++ include/asura/core/log.h
@@ -1,10 +1,16 @@
+//
+// Created by Shreejit Murthy on 13/12/2025
+//
+
+#pragma once
+
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 
 #include <memory>
 
 namespace Asura::Log {
-static inline spdlog::logger& get() {
+inline spdlog::logger& get() {
     static auto logger = []{
         auto l = spdlog::stdout_color_mt("logsura");
         l->set_pattern("\033[2m[%T.%e | +%2oms]\033[0m [%^%l%$] %v");
@@ -12,4 +18,4 @@ static inline spdlog::logger& get() {
     }();
     return *logger;
 }
-} // Asura
+} // Asura::Log
