581e838c10333b7c0b802c4d8d1ae98977216608
Shreejit Murthy <botmorph17@gmail.com>
2025-12-30 01:12:48 +0530

    refactor logging structure to use stderr and encapsulate logger in Log class

--- include/asura/core/log.h
+++ include/asura/core/log.h
@@ -9,13 +9,21 @@
 
 #include <memory>
 
-namespace Asura::Log {
-inline spdlog::logger& get() {
-    static auto logger = []{
-        auto l = spdlog::stdout_color_mt("logsura");
-        l->set_pattern("\033[2m[%T.%e | +%2oms]\033[0m [%^%l%$] %v");
-        return l;
-    }();
-    return *logger;
-}
-} // Asura::Log
+#define LOGSURA_INFO(...)   Asura::Log::get().info(__VA_ARGS__)
+#define LOGSURA_WARN(...)   Asura::Log::get().warn(__VA_ARGS__)
+#define LOGSURA_ERROR(...)  Asura::Log::get().error(__VA_ARGS__)
+#define LOGSURA_CRIT(...)   Asura::Log::get().critical(__VA_ARGS__)
+
+namespace Asura {
+class Log {
+public:
+    static spdlog::logger& get() {
+        static auto logger = []{
+            auto l = spdlog::stderr_color_mt("logsura");
+            l->set_pattern("\033[2m[%T.%e | +%2oms]\033[0m [%^%l%$] %v");
+            return l;
+        }();
+        return *logger;
+    }
+};
+} // Asura
