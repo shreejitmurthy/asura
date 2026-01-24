fa174822fec0be0d7bceab3eaffd3ca35795140d
Shreejit Murthy <botmorph17@gmail.com>
2026-01-25 04:53:37 +1030

    minor setting up

--- include/asura/gfx/model.hh
+++ include/asura/gfx/model.hh
@@ -7,18 +7,30 @@
 
 namespace Asura::Model {
 
-// Metal and D3D11 support storage buffers, as well as OpenGL 4.1+.
-// This can very easily be checked at runtime with `sg_query_features().compute`
+/* 
+ * Metal and D3D11 support storage buffers, as well as OpenGL 4.1+.
+ * This can very easily be checked at runtime with `sg_query_features().compute`
+ * Skeletal is just using the immediate mode debug renderer to draw the lines of the skeleton, 
+ * showing the joints and how they move. 
+ */
 enum class RenderMethod {
     HardwareInstance,
-    StorageBuffer
+    StorageBuffer,
+    Skeletal
 };
 
 class Renderer {
 public:
     void init();
 
-    void push();
+    void push(RenderMethod rmethod) {
+        /* 
+         * Skeletal renders are done with immediate-mode OpenGL calls and therefore
+         * cannot be instanced or rendered with storage buffers, so we'll just draw the
+         * skeleton itself here.
+         */
+        if (rmethod == RenderMethod::Skeletal) draw_skel();
+    }
 
     void render();
 private:
@@ -28,7 +40,8 @@ private:
     void init_stbuf();
     void draw_stbuf();
 
-    RenderMethod rmethod;
+    void init_skel(const char* model_path, const char* anim_path);
+    void draw_skel();
 };
 
 }
